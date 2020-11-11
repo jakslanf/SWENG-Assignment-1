ff45f71fce9e92673518c1e640a8a8a3deb43100
jakslanf <72381232+jakslanf@users.noreply.github.com>
2020-11-11 10:24:05 +0000

    DAG Initial Implementation
    added DAG support to Node class.
    Created a fancy new DAG LCA class

---
+++ src/DirectedAcyclicGraph.java
@@ -0,0 +1,7 @@
+
+public class DirectedAcyclicGraph
+{
+	Node root;
+	
+
+}
--- src/Node.java
+++ src/Node.java
@@ -1,11 +1,45 @@
-	class Node
+import java.util.ArrayList;
+
+class Node
 	{ 
 	    int data; 
-	    Node left, right; 
-	  
+	    Node left, right; //Binary Tree
+	    ArrayList<Node> children; //Directed Acyclic Graph
 	    Node(int value)
 	    { 
 	        data = value; 
-	        left = right = null; 
-	    } 
+	        left = right = null;
+	        children = new ArrayList<Node>();
+	    }
+	    
+	    public Node getChild(int i)
+	    {
+	    	if(i < children.size() - 1)
+	    	{
+	    		return children.get(i);
+	    	}
+	    	else
+	    	{
+	    		return null;
+	    	}
+	    }
+	    
+	    public void addChild(Node child)
+	    {
+	    	children.add(child);
+	    }
+	    
+	    public Node setChild(int i, Node child)
+	    {
+	    	if(i < children.size() - 1)
+	    	{
+	    		Node oldValue = children.get(i);
+	    		children.set(i, child);
+	    		return oldValue;
+	    	}
+	    	else
+	    	{
+	    		return null;
+	    	}
+	    }
 	} 
